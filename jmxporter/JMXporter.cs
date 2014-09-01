5ade00a4bef7584e4a440ab2fe0827b9a78f5e24
Gilang Jalu <gilangjalu@yahoo.com>
2014-09-02 00:53:34 +0800

    Add basic implementation

---
+++ jmxporter/JMXporter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Fiddler;
+// Fiddler minimum version required
+[assembly: Fiddler.RequiredVersion("4.4.9.3")]
+
+namespace jmxporter
+{
+    [ProfferFormat("JMeter", "JMeter .jmx Format")]
+    public class JMXporter : ISessionExporter
+    {
+        public bool ExportSessions(string sFormat, Session[] oSessions, Dictionary<string, object> dictOptions,
+            EventHandler<ProgressCallbackEventArgs> evtProgressNotifications)
+        {
+            bool bResult = true;
+            string sFilename = null;
+
+            sFilename = Fiddler.Utilities.ObtainSaveFilename("Export As " + sFormat, "JMeter Files (*.jmx)|*.jmx");
+
+            if (String.IsNullOrEmpty(sFilename)) return false;
+
+            if (!Path.HasExtension(sFilename)) sFilename = sFilename + ".jmx";
+
+            try
+            {
+                Encoding encUTF8NoBOM = new UTF8Encoding(false);
+
+                JMeterTestPlan jMeterTestPlan = new JMeterTestPlan(oSessions, sFilename);
+                System.IO.StreamWriter sw = new StreamWriter(sFilename, false, encUTF8NoBOM);
+
+                sw.Write(jMeterTestPlan.Jmx);
+                sw.Close();
+
+                Fiddler.FiddlerApplication.Log.LogString("Successfully exported sessions to JMeter Test Plan");
+                Fiddler.FiddlerApplication.Log.LogString(String.Format("\t{0}", sFilename));
+            }
+            catch (Exception eX)
+            {
+                Fiddler.FiddlerApplication.Log.LogString(eX.Message);
+                Fiddler.FiddlerApplication.Log.LogString(eX.StackTrace);
+                bResult = false;
+            }
+
+            return bResult;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ jmxporter/JMeterTestPlan.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using System.IO;
+
+namespace jmxporter
+{
+    public class JMeterTestPlan
+    {
+        private SessionList sessionList;
+        private Fiddler.Session[] sessions;
+
+        public JMeterTestPlan()
+        {
+            sessions = new Fiddler.Session[0];
+            sessionList = new SessionList(sessions);
+        }
+
+        public JMeterTestPlan(Fiddler.Session[] oSessions, string outputFilename)
+        {
+            this.sessions = oSessions;
+            sessionList = new SessionList(oSessions);
+        }
+
+        public string Jmx 
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+                XDocument doc = XDocument.Parse(this.Xml);
+                sb.Append(doc.ToString());
+                return sb.ToString();
+            }
+        }
+
+        private string Xml 
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<jmeterTestPlan version=\"1.2\" properties=\"2.3\">");
+                sb.Append(sessionList.Xml);
+                sb.Append("</jmeterTestPlan>");
+                return sb.ToString();
+            }
+        }
+    }
+
+    public class SessionList
+    {
+        private Fiddler.Session[] sessions;
+
+        public SessionList()
+        {
+            sessions = new Fiddler.Session[0];
+        }
+
+        public SessionList(Fiddler.Session[] oSessions)
+        {
+            this.sessions = oSessions;
+        }
+
+        public string Xml {
+            get 
+            {
+                StringBuilder sb = new StringBuilder();
+                if (sessions.Length > 0)
+                {
+                    sb.Append("<hashTree>");
+                    foreach (Fiddler.Session session in sessions)
+                    {
+                        HTTPSamplerProxy httpSamplerProxy = new HTTPSamplerProxy(session);
+                        sb.Append(httpSamplerProxy.Xml);
+                    }
+                    sb.Append("</hashTree>");
+                }
+                return sb.ToString();
+            }
+        }
+    }
+
+    public class HTTPSamplerProxy
+    {
+        Fiddler.Session session;
+
+        public HTTPSamplerProxy(Fiddler.Session session)
+        {
+            this.session = session;
+        }
+
+        public string Xml
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(String.Format("<HTTPSamplerProxy guiclass=\"HttpTestSampleGui\" "
+                    + "testclass=\"HTTPSamplerProxy\" testname=\"{0}\" enabled=\"true\">",
+                    Path));
+                sb.Append("<boolProp name=\"HTTPSampler.postBodyRaw\">true</boolProp>");
+                sb.Append("<elementProp name=\"HTTPSampler.Arguments\" elementType=\"Arguments\">");
+                sb.Append("<collectionProp name=\"Arguments.arguments\">");
+                sb.Append("<elementProp name=\"\" elementType=\"HTTPArgument\">");
+                sb.Append("<boolProp name=\"HTTPArgument.always_encode\">false</boolProp>");
+                sb.Append(String.Format("<stringProp name=\"Argument.value\">{0}</stringProp>", RequestBody));
+                sb.Append("<stringProp name=\"Argument.metadata\">=</stringProp>");
+                sb.Append("</elementProp>");
+                sb.Append("</collectionProp>");
+                sb.Append("</elementProp>");
+                sb.Append(String.Format("<stringProp name=\"HTTPSampler.domain\">{0}</stringProp>", session.host));
+                sb.Append(String.Format("<stringProp name=\"HTTPSampler.port\">{0}</stringProp>", Port));
+                sb.Append("<stringProp name=\"HTTPSampler.connect_timeout\"></stringProp>");
+                sb.Append("<stringProp name=\"HTTPSampler.response_timeout\"></stringProp>");
+                sb.Append(String.Format("<stringProp name=\"HTTPSampler.protocol\">{0}</stringProp>",
+                    session.oRequest.headers.UriScheme));
+                sb.Append("<stringProp name=\"HTTPSampler.contentEncoding\"></stringProp>");
+                sb.Append(String.Format("<stringProp name=\"HTTPSampler.path\">{0}</stringProp>", Path));
+                sb.Append(String.Format("<stringProp name=\"HTTPSampler.method\">{0}</stringProp>", 
+                    session.oRequest.headers.HTTPMethod.ToUpper()));
+                sb.Append("<boolProp name=\"HTTPSampler.follow_redirects\">true</boolProp>");
+                sb.Append("<boolProp name=\"HTTPSampler.auto_redirects\">false</boolProp>");
+                sb.Append("<boolProp name=\"HTTPSampler.use_keepalive\">true</boolProp>");
+                sb.Append("<boolProp name=\"HTTPSampler.DO_MULTIPART_POST\">false</boolProp>");
+                sb.Append("<boolProp name=\"HTTPSampler.monitor\">false</boolProp>");
+                sb.Append("<stringProp name=\"HTTPSampler.embedded_url_re\"></stringProp>");
+                sb.Append("</HTTPSamplerProxy>");
+                sb.Append("<hashTree/>");
+                return sb.ToString();
+            }
+        }
+
+        private string Path
+        {
+            get
+            {
+                return System.Net.WebUtility.HtmlEncode(session.PathAndQuery);
+            }
+        }
+
+        private string getPort()
+        {
+            int port = session.port;
+            string protocol = session.oRequest.headers.UriScheme;
+            if (protocol.ToLower() == ("https") && port == 443)
+            {
+                return "";
+            }
+            if (protocol.ToLower() == ("http") && port == 80)
+            {
+                return "";
+            }
+            return port.ToString();
+        }
+
+        private string Port
+        {
+            get
+            {
+                return getPort();
+            }
+        }
+
+        private string RequestBody
+        {
+            get
+            {
+                return System.Net.WebUtility.HtmlEncode(session.GetRequestBodyAsString());
+            }
+        }
+    }
+}
